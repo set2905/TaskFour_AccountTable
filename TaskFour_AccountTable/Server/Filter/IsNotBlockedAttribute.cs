261f2806df9d7865b58316850027c6c5f2706b52
set2905 <72752613+set2905@users.noreply.github.com>
2023-08-08 01:40:15 +0400

    Create IsNotBlockedAttribute.cs

---
+++ TaskFour_AccountTable/Server/Filter/IsNotBlockedAttribute.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using Microsoft.AspNetCore.Identity;
+using TaskFour_AccountTable.Server.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace TaskFour_AccountTable.Server.Filter
+{
+
+    public class IsNotBlockedAttribute : Attribute, IAuthorizationFilter
+    {
+        private readonly UserManager<User> userManager;
+
+        public IsNotBlockedAttribute(UserManager<User> userManager)
+        {
+            this.userManager=userManager;
+        }
+
+        public async void OnAuthorization(AuthorizationFilterContext context)
+        {
+            //string token = context.HttpContext.Request.Headers[HeaderNames.Authorization].ToString().Replace("Bearer ", "");
+            string? userName = context.HttpContext.User.Identity.Name;
+            User? user = await userManager.FindByNameAsync(userName);
+            if (user==null)
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            if (user.IsBlocked)
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+        }
+
+    }
+}
