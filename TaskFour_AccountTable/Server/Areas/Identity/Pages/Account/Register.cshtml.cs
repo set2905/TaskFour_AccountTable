a8fed1ed205df47e33a4ee64e56000c7adf85f47
set2905 <72752613+set2905@users.noreply.github.com>
2023-08-10 00:54:15 +0400

    Update Register.cshtml.cs

--- TaskFour_AccountTable/Server/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ TaskFour_AccountTable/Server/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -62,7 +62,7 @@ namespace TaskFour_AccountTable.Server.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            returnUrl = Url.Content("~/");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
